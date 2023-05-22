f2065725dde4a94547d9262388484a08f224c894
lilpumpy19 <100938542+lilpumpy19@users.noreply.github.com>
2023-05-22 19:20:31 +0400

    02
    работают вакансии (без фото)

--- src/main/java/com/example/springproject/configs/VacancyConfig.java
+++ src/main/java/com/example/springproject/configs/VacancyConfig.java
@@ -16,7 +16,11 @@ public class VacancyConfig {
             Vacancy vacancy = new Vacancy("грузчик","саратов",20000);
             Vacancy vacancy1 = new Vacancy("повар","саратов",30000);
             Vacancy vacancy2 = new Vacancy("разработчик it","саратов",80000);
-            repository.saveAll(List.of(vacancy,vacancy1,vacancy2));
+            Vacancy vacancy3 = new Vacancy("разработчик it","москва",100000);
+            Vacancy vacancy4= new Vacancy("экономист","саратов",66000);
+            Vacancy vacancy5 = new Vacancy("разработчик it","спб",120000);
+            Vacancy vacancy6 = new Vacancy("уборщик","саратов",10000);
+            repository.saveAll(List.of(vacancy,vacancy1,vacancy2,vacancy4,vacancy5,vacancy6,vacancy3));
         };
     }
 }
